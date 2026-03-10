a37fe729aac2c7fc5b8bc7454a7767e8ba0e09a7
Bruno Medeiros <bruno.medeiros@cryptio.co>
2026-03-10 16:22:34 +0000

    Implement LimitOrderBook trait for both V0 and V1 modules, adding methods for order management and market operations.

--- limit-order-book/src/lib.rs
+++ limit-order-book/src/lib.rs
@@ -1,8 +1,10 @@
 mod book_v0;
 mod book_v1;
+mod book_api;
 pub mod event;
 mod order;
 pub mod types;
 
+pub use book_api::LimitOrderBook;
 pub use book_v0::book::LimitOrderBookV0;
 pub use book_v1::book::LimitOrderBookV1;
